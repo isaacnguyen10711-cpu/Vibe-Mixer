b04740043250a78a05c59d38b10d1103bb89b023
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-24 16:20:29 +0930

    Installed sqlmodel and changed the code to use sqlmodel instead of sqlalchemy

--- mm-backend/app/database.py
+++ mm-backend/app/database.py
@@ -1,16 +1,10 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.config import settings
 
-#The engine owns the connection pool used to communicate with Neon PostgreSQL.
-engine = create_async_engine(settings.DATABASE_URL, echo=False)
+#Create an async engine that will be used to connect to the database.
+engine = create_async_engine(settings.DATABASE_URL, pool_pre_ping=True, echo=False)
 
-#This factory creates a new asynchronous database session for each request.
-SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, autoflush=False, expire_on_commit=False,
-)
-
-#All future SQLAlchemy entity models inherit from this base.
-class Base(DeclarativeBase):
-    pass
- 
+#This creates a new async SQLModel session for each request.
+SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, autoflush=False, expire_on_commit=False)
--- mm-backend/app/dependencies.py
+++ mm-backend/app/dependencies.py
@@ -1,17 +1,15 @@
 from collections.abc import AsyncIterator
-from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Depends
 from typing import Annotated
-
-
+from fastapi import Depends
+from sqlmodel.ext.asyncio.session import AsyncSession
 from app.database import SessionLocal
 
 
-#Open one session for a request and close it automatically when the request ends.
+#Open one async session for a request and close it when the request ends.
 async def get_db() -> AsyncIterator[AsyncSession]:
     async with SessionLocal() as session:
         yield session
 
 
-#Routers can use `db: DatabaseSession` to receive the request's session.
+#Dependency that can be injected into a router
 DatabaseSession = Annotated[AsyncSession, Depends(get_db)]
