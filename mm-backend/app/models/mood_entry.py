a6964756d46b0396c7300ec91d7945252540ead8
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-27 03:20:36 +0930

    Added check constraint to explicitly prevent data out of the range being added to the db

--- mm-backend/app/models/mood_entry.py
+++ mm-backend/app/models/mood_entry.py
@@ -1,10 +1,19 @@
 from sqlmodel import SQLModel, Field
+from sqlalchemy import CheckConstraint
 from typing import ClassVar
 from datetime import UTC, datetime
 from enum import StrEnum
 
 class MoodEntry (SQLModel, table=True):
     __tablename__: ClassVar[str ]= "mood_entries"
+    __table_args__ = (
+        CheckConstraint("happy BETWEEN 1 AND 5", name="ck_mood_entries_happy_range"),
+        CheckConstraint("energetic BETWEEN 1 AND 5", name="ck_mood_entries_energetic_range"),
+        CheckConstraint("calm BETWEEN 1 AND 5", name="ck_mood_entries_calm_range"),
+        CheckConstraint("anxious BETWEEN 1 AND 5", name="ck_mood_entries_anxious_range"),
+        CheckConstraint("sad BETWEEN 1 AND 5", name="ck_mood_entries_sad_range"),
+        CheckConstraint("angry BETWEEN 1 AND 5", name="ck_mood_entries_angry_range"),
+    )
     
     #Generate columns for the mood_entries table in the database.
     id: int | None = Field(default=None, primary_key=True)
