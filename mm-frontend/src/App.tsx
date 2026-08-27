b2c94c028ec2f27ec0045ac958666a8955e3725c
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-27 16:47:39 +0930

    Updated homepage with ButtonRow component

--- mm-frontend/src/App.tsx
+++ mm-frontend/src/App.tsx
@@ -3,7 +3,7 @@ import HomePage from './pages/HomePage'
 function App() {
 
  return (
-  <div className="bg-linear-to-r from-red-100 to-violet-300 min-h-screen max-w-screen flex justify-center">
+  <div className="bg-linear-to-r from-violet-200 to-red-200 min-h-screen max-w-screen flex justify-center">
     <HomePage/>
   </div>
  )
--- mm-frontend/src/pages/HomePage.tsx
+++ mm-frontend/src/pages/HomePage.tsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react'
+import ButtonRow from '../components/ButtonRow';
 
 function HomePage() {
-    const [mood, setMood] = useState(1);
+    const [happy, setHappy] = useState(1);
+    const [energetic, setEnergetic] = useState(1);
+    const [calm, setCalm] = useState(1);
+    const [sad, setSad] = useState(1);
+    const [anxious, setAnxious] = useState(1);
+    const [angry, setAngry] = useState(1);
 
     return (
-        <div className="max-w-5xl mx-auto">
+        <div className="max-w-6xl mx-auto">
             <div className="flex justify-center">
                 <h1 className="text-lg mt-20 italic underline font-medium md:text-xl lg:text-2xl">Mood Mixer</h1>
             </div>
@@ -12,73 +18,58 @@ function HomePage() {
                 <h1 className="text-2xl mt-5 font-bold md:text-3xl lg:text-5xl">How are you feeling today?</h1>
             </div>
 
-            <div className="flex justify-center mt-10">
-                <h1 className="text-lg font-medium md:text-xl lg:text-2xl">Happy</h1>
+            <div className="grid md:grid-cols-3 gap-20 justify-center mt-10">
+                <div>
+                    <div className="flex justify-center">
+                        <h1 className="text-lg font-medium md:text-xl lg:text-2xl">Happy</h1>
+                    </div>
+                    <div className="grid grid-cols-5 gap-2 justify-center mt-3">
+                        <ButtonRow onMoodSelect={setHappy} />
+                    </div>
+                </div>
+                <div>
+                    <div className="flex justify-center">
+                        <h1 className="text-lg font-medium md:text-xl lg:text-2xl">Energetic</h1>
+                    </div>
+                    <div className="grid grid-cols-5 gap-2 justify-center mt-3">
+                        <ButtonRow onMoodSelect={setEnergetic} />
+                    </div>
+                </div>
+                <div>
+                    <div className="flex justify-center">
+                        <h1 className="text-lg font-medium md:text-xl lg:text-2xl">Calm</h1>
+                    </div>
+                    <div className="grid grid-cols-5 gap-2 justify-center mt-3">
+                        <ButtonRow onMoodSelect={setCalm} />
+                    </div>
+                </div>
+                <div>
+                    <div className="flex justify-center">
+                        <h1 className="text-lg font-medium md:text-xl lg:text-2xl">Sad</h1>
+                    </div>
+                    <div className="grid grid-cols-5 gap-2 justify-center mt-3">
+                        <ButtonRow onMoodSelect={setSad} />
+                    </div>
+                </div>
+                <div>
+                    <div className="flex justify-center">
+                        <h1 className="text-lg font-medium md:text-xl lg:text-2xl">Anxious</h1>
+                    </div>
+                    <div className="grid grid-cols-5 gap-2 justify-center mt-3">
+                        <ButtonRow onMoodSelect={setAnxious} />
+                    </div>
+                </div>
+                <div>
+                    <div className="flex justify-center">
+                        <h1 className="text-lg font-medium md:text-xl lg:text-2xl">Angry</h1>
+                    </div>
+                    <div className="grid grid-cols-5 gap-2 justify-center mt-3">
+                        <ButtonRow onMoodSelect={setAngry} />
+                    </div>
+                </div>
             </div>
-            <div className="grid grid-cols-5 gap-4 md:grid-cols-10 justify-center mt-5">
-                <button
-                    className="px-4 py-2 mx-2 rounded hover:bg-violet-300 outline outline-2 outline-pink-300 transition duration-500 ease-in-out hover:scale-110"
-                    onClick={() => setMood(1)}
-                >
-                    1
-                </button>
-                <button
-                    className="px-4 py-2 mx-2 rounded hover:bg-violet-300 outline outline-2 outline-pink-300 transition duration-500 ease-in-out hover:scale-110"
-                    onClick={() => setMood(2)}
-                >
-                    2
-                </button>
-                <button
-                    className="px-4 py-2 mx-2 rounded hover:bg-violet-300 outline outline-2 outline-pink-300 transition duration-500 ease-in-out hover:scale-110"
-                    onClick={() => setMood(3)}
-                >
-                    3
-                </button>
-                <button
-                    className="px-4 py-2 mx-2 rounded hover:bg-violet-300 outline outline-2 outline-pink-300 transition duration-500 ease-in-out hover:scale-110"
-                    onClick={() => setMood(4)}
-                >
-                    4
-                </button>
-                <button
-                    className="px-4 py-2 mx-2 rounded hover:bg-violet-300 outline outline-2 outline-pink-300 transition duration-500 ease-in-out hover:scale-110"
-                    onClick={() => setMood(5)}
-                >
-                    5
-                </button>
-                <button
-                    className="px-4 py-2 mx-2 rounded hover:bg-violet-300 outline outline-2 outline-pink-300 transition duration-500 ease-in-out hover:scale-110"
-                    onClick={() => setMood(6)}
-                >
-                    6
-                </button>
-                <button
-                    className="px-4 py-2 mx-2 rounded hover:bg-violet-300 outline outline-2 outline-pink-300 transition duration-500 ease-in-out hover:scale-110"
-                    onClick={() => setMood(7)}
-                >
-                    7
-                </button>
-                <button
-                    className="px-4 py-2 mx-2 rounded hover:bg-violet-300 outline outline-2 outline-pink-300 transition duration-500 ease-in-out hover:scale-110"
-                    onClick={() => setMood(8)}
-                >
-                    8
-                </button>
-                <button
-                    className="px-4 py-2 mx-2 rounded hover:bg-violet-300 outline outline-2 outline-pink-300 transition duration-500 ease-in-out hover:scale-110"
-                    onClick={() => setMood(9)}
-                >
-                    9
-                </button>
-                <button
-                    className="px-4 py-2 mx-2 rounded hover:bg-violet-300 outline outline-2 outline-pink-300 transition duration-500 ease-in-out hover:scale-110"
-                    onClick={() => setMood(10)}
-                >
-                    10
-                </button>
-            </div>
-
         </div>
+
     )
 }
 
