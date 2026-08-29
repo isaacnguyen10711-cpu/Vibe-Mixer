7a4701e80263338e7bdec0e73891602d5f946c4e
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-29 22:16:34 +0930

    Made username optional for User Object

--- mm-backend/app/models/user.py
+++ mm-backend/app/models/user.py
@@ -7,20 +7,19 @@ class User(SQLModel, table=True):
     
     #Generate columns for the users table in the database.
     id: int | None = Field(default=None, primary_key=True)
-    username: str = Field(max_length=50, unique=True, index=True)
+    username: str | None = Field(default=None, max_length=50, unique=True, index=True)
     email: str = Field(max_length=100, unique=True, index=True)
     hashed_password: str = Field(max_length=255) 
     created_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
     
 #for user registration request
 class UserRegistrationRequest(SQLModel):
-    username: str = Field(max_length=50)
     email: str = Field(max_length=100)
     password: str = Field(min_length=8, max_length=128)
 
 class UserResponse(SQLModel):
     id: int
-    username: str
+    username: str | None
     email: str
     created_at: datetime
 
