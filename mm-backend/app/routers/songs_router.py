b7127272ef204441ce9c7cd89f2fed7145728903
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-31 02:33:58 +0930

    Fixed get_songs to include userId check

--- mm-backend/app/routers/songs_router.py
+++ mm-backend/app/routers/songs_router.py
@@ -12,7 +12,13 @@ router = APIRouter(
 
 @router.get("/get-songs/{playlist_id}")
 async def get_songs(playlist_id: int, db: DatabaseSession, user: AuthorizedUser):
-    query = select(Songs).where(Songs.playlist_id == playlist_id)
-    result = await db.exec(query)
+    playlist_query = select(Playlist).where(Playlist.id == playlist_id, Playlist.user_id == user.id)
+    playlist_result = await db.exec(playlist_query)
+    playlist = playlist_result.first()
+    if playlist is None:
+        raise HTTPException(status_code=404, detail="Playlist not found.")
+    
+    songs_query = select(Songs).where(Songs.playlist_id == playlist_id)
+    result = await db.exec(songs_query)
     songs = result.all()
     return songs
