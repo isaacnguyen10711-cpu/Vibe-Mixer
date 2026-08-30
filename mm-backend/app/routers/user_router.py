dc629e88cd97177701619bb6d88d59b6d5c36117
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-30 17:24:27 +0930

    Fixed /profile route

--- mm-backend/app/routers/user_router.py
+++ mm-backend/app/routers/user_router.py
@@ -1,13 +1,18 @@
-from fastapi import APIRouter
-from app.dependencies import AuthorizedUser
+from fastapi import APIRouter, Depends, HTTPException
+from app.dependencies import AuthorizedUser, DatabaseSession
 from app.models.user import UserResponse, User
+from sqlmodel import select
 
 router = APIRouter(
     prefix="/users",
     tags=["users"]
 )
 
-@router.get("/profile", response_model=UserResponse)
-async def view_profile(current_user: AuthorizedUser) -> User:
-    return current_user
+@router.get("/profile")
+async def view_profile(current_user: AuthorizedUser, db: DatabaseSession) -> UserResponse:
+    return UserResponse(
+        id=current_user.id,
+        username=current_user.username,
+        email=current_user.email
+    )
     
