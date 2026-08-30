196e3799055065ee7f8487fc685fff9d06242b53
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-31 00:34:00 +0930

    Added fetchProfile function to run when the page first load and return the user data

--- mm-frontend/src/pages/ProfilePage.tsx
+++ mm-frontend/src/pages/ProfilePage.tsx
@@ -1,6 +1,42 @@
 import { Link } from "react-router";
+import { useState, useEffect } from "react";
 
 function ProfilePage() {
+    const [username, setUsername] = useState("Not set yet");
+    const [email, setEmail] = useState("you@example.com");
+    const [createdAt, setCreatedAt] = useState("Current Date");
+    
+    useEffect(()  => {
+        const fetchProfile = async () => {
+            try {
+            const response = await fetch("http://127.0.0.1:8000/users/profile", 
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("access_token")}`
+                    }
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch profile information.");
+            }
+
+            const data = await response.json();
+            setUsername(data.username);
+            setEmail(data.email);
+            setCreatedAt(data.created_at);
+            } 
+            catch (error) {
+                if (error instanceof Error) {
+                    console.error("Error fetching profile:", error.message);
+                }
+                alert("An error occurred while fetching profile information.");
+            }
+        };
+        fetchProfile();
+    }, []);
+
+
     return (
         <main className="flex min-h-screen w-full items-center justify-center px-4 py-8 md:px-8 lg:px-12">
             <section className="w-full max-w-sm rounded-2xl border-2 border-violet-300 bg-white/75 p-6 shadow-lg md:max-w-md md:p-8 lg:max-w-lg lg:p-9">
@@ -25,7 +61,7 @@ function ProfilePage() {
                             Username
                         </p>
                         <p className="mt-1 text-sm text-gray-800 md:text-base">
-                            Not set yet
+                            {username}
                         </p>
                     </div>
 
@@ -34,7 +70,7 @@ function ProfilePage() {
                             Email
                         </p>
                         <p className="mt-1 break-words text-sm text-gray-800 md:text-base">
-                            you@example.com
+                            {email}
                         </p>
                     </div>
 
@@ -43,7 +79,7 @@ function ProfilePage() {
                             Member since
                         </p>
                         <p className="mt-1 text-sm text-gray-800 md:text-base">
-                            Date will appear here
+                            {new Date(createdAt).toLocaleDateString()}
                         </p>
                     </div>
                 </div>
