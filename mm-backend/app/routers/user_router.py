0dd136f6eceaa4d6eaae3f45d29b3abc836afafe
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-08-30 17:23:51 +0930

    Created /profile route to allow users to view their profile

---
+++ mm-backend/app/routers/user_router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from app.dependencies import AuthorizedUser
+from app.models.user import UserResponse, User
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"]
+)
+
+@router.get("/profile", response_model=UserResponse)
+async def view_profile(current_user: AuthorizedUser) -> User:
+    return current_user
+    
