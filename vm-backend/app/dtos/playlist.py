1f7f950b0fd8148ddb3dc3a4fa26aedf1a575c50
Isaac Nguyen <isaac.nguyen10711@gmail.com>
2026-09-15 13:30:35 +0930

    Add length validation to playlist DTOs

--- vm-backend/app/dtos/playlist.py
+++ vm-backend/app/dtos/playlist.py
@@ -1,21 +1,21 @@
 from datetime import datetime
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, Field
 from app.dtos.songs import GeneratedSong
 
 #For generated playlist response from OpenAI
 class GeneratedPlaylist(SQLModel):
-    name: str
-    description: str
+    name: str = Field(max_length=100)
+    description: str = Field(max_length=255)
     songs: list[GeneratedSong]
 
 #For playlist response from the database to return in user profile
 class PlaylistResponse(SQLModel):
     id: int
-    name: str
-    description: str | None
+    name: str = Field(max_length=100)
+    description: str | None = Field(max_length=255)
     created_at: datetime
 
 
 class PlaylistUpdateRequest(SQLModel):
-    name: str
-    description: str
+    name: str = Field(max_length=100)
+    description: str = Field(max_length=255)
